f45635255f89332f8a863c0dc7ae576293621209
LENA EIVY <applena@gmail.com>
2018-12-30 13:37:01 -0800

    test outlines

---
+++ _tests_/chatroom.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+describe('establishes a server and allows users to join', () => {
+  it('assigns new users a uuid', () => {
+
+  });
+
+  it('assigns new users with a nickname', () => {
+
+  });
+});
+
+describe('parses data that the user sends', () => {
+  it('', () => {
+
+  });
+
+  it('', () => {
+
+  });
+
+  it('', () => {
+
+  });
+});
+
+describe('allows users to run different actions', () => {
+  it('sends a message to everyone connected', () => {
+
+  });
+
+  it('prints out a list of everyone who is connected', () => {
+
+  });
+
+  it('allows you to change your name', () => {
+
+  });
+
+  it('quits the chatroom', () => {
+
+  });
+
+  it('sends a direct message to a user of your choice', () => {
+
+  });
+
+  it('requires you to enter the nickname of the person you want to direct message', () => {
+
+  });
+});
+
+describe('establishes a server and allows users to join', () => {
+  it('', () => {
+
+  });
+
+  it('', () => {
+
+  });
+
+  it('', () => {
+
+  });
+});
