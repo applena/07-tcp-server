a28f08a527d7ae754a9beb4d089597d95b85927b
LENA EIVY <applena@gmail.com>
2019-01-01 10:34:29 -0800

    lab working - no tests yet

--- _tests_/chatroom.test.js
+++ _tests_/chatroom.test.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const socketPool = require('../lib/socketPool.js');
+//const events = require('../lib/events.js');
+const dm = require('../lib/actions/atDm.js');
+
 describe('establishes a server and allows users to join', () => {
   it('assigns new users a uuid', () => {
 
@@ -23,6 +27,30 @@ describe('parses data that the user sends', () => {
 
   });
 });
+
+// describe('@dm action', () => {
+
+//   it('sends nothing if the target user is not valide', () => {
+//     let commandObject = {
+//       command:'@dm',
+//       target:'nobody',
+//       message:'Hello',
+//     };
+//     dm(commandObject, 1);
+//     expect(socketPool.write).not.toHaveBeenCalled();
+//   });
+
+//   it('sends a message to a valid user', () => {
+//     let commandObject = {
+//       command:'@dm',
+//       target:'two',
+//       message:'Hello',
+//     };
+//     dm(commandObject, 1);
+//     expect(socketPool.write).toHaveBeenCalled();
+//   });
+
+// });
 
 describe('allows users to run different actions', () => {
   it('sends a message to everyone connected', () => {
@@ -63,3 +91,7 @@ describe('establishes a server and allows users to join', () => {
 
   });
 });
+
+
+
+
